79f6ba48df0dd4fa9e2810ef29bb5702d57c7da1
Will保哥 <doggy.huang@gmail.com>
2017-04-15 13:22:03 +0800

    08 加入 Charts 子路由 ( /charts/flot 與 /charts/radial )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
+import { ReduceSubscriber } from 'rxjs/operator/reduce';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
 import { DashboardComponent } from "app/dashboard/dashboard.component";
 import { CardsComponent } from "app/cards/cards.component";
+import { FlotComponent } from "app/charts/flot/flot.component";
+import { RadialComponent } from "app/charts/radial/radial.component";
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'cards', component: CardsComponent }
+  { path: 'cards', component: CardsComponent },
+  { path: 'charts',
+    children: [
+      { path: '', redirectTo: 'flot', pathMatch: 'full' },
+      { path: 'flot', component: FlotComponent },
+      { path: 'radial', component: RadialComponent }
+    ]
+  }
 ];
 
 @NgModule({
