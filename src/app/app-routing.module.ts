c8ce5726f381d7270e9084755b5c4e3ee6a7a074
Will保哥 <doggy.huang@gmail.com>
2017-04-15 11:42:12 +0800

    06 練習將靜態版面改成 Angular 應用程式，並設定路由建立真正的 SPA 應用程式

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Route } from '@angular/router';
+import { DashboardComponent } from "app/dashboard/dashboard.component";
+import { CardsComponent } from "app/cards/cards.component";
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'cards', component: CardsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
