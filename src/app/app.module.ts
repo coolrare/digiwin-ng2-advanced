deee015d171a1abe02ef76d67c971d619e20f12a
Will保哥 <doggy.huang@gmail.com>
2017-04-21 09:56:04 +0800

    從 AppModule 匯入 ReactiveFormsModule 模組

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -24,6 +24,7 @@ import { LoginGuard } from './login.guard';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
