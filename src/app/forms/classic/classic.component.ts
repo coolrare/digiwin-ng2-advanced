e1a10f6a8b8d84563890bf19f419577868ca1629
Will保哥 <doggy.huang@gmail.com>
2017-04-21 14:42:21 +0800

    22 練習設計 FormArray 動態欄位數量的表單

--- src/app/forms/classic/classic.component.ts
+++ src/app/forms/classic/classic.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormBuilder, FormGroup } from "@angular/forms";
+import { NgForm, FormBuilder, FormGroup, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-classic',
@@ -13,10 +13,22 @@ export class ClassicComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit() {
+    this.metadataControls = this.fb.array([
+        this.fb.control('Temp 1'),
+        this.fb.control('Temp 2')
+      ]);
+
     this.form = this.fb.group({
-      'title': 'Default Title',
-      'summary': 'My Summary'
+      'title': '',
+      'summary': '',
+      'metadata': this.metadataControls
     });
+  }
+
+  metadataControls: FormArray;
+
+  addNewControl() {
+    this.metadataControls.insert(0, this.fb.control('TEST'));
   }
 
   data: any = {};
