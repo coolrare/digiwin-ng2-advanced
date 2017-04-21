e04c5c606b3bf9e83f55fe1983fb15df5e691d02
Will保哥 <doggy.huang@gmail.com>
2017-04-21 09:36:07 +0800

    示範驗證登入失敗就轉向到登入頁面

--- src/app/login.guard.ts
+++ src/app/login.guard.ts
@@ -1,15 +1,18 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
+  constructor(private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if(next.queryParams['apikey'] == '123') {
       return true;
     } else {
+      this.router.navigateByUrl('/login');
       return false;
     }
   }
